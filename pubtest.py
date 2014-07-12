bd835823d70e740461d3321b1f387f8b3e55fc0a
ross128 <ross128@geekmail.de>
2014-07-12 15:01:13 +0200

    added test client for publishing data

---
+++ pubtest.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import zmq
+import argparse
+
+def main(host, port, topic):
+	context = zmq.Context.instance()
+	publisher = context.socket(zmq.PUB)
+	publisher.connect("tcp://{0}:{1}".format(host, port))
+
+	running = True
+	print "press [CTRL] + [D] to quit"
+	while running:
+		try:
+			data = raw_input("in> ")
+		except EOFError:
+			break
+		publisher.send_multipart([topic, data])
+
+	# clean up
+	publisher.close()
+	context.term()
+
+if __name__ == "__main__":
+	argparser = argparse.ArgumentParser(
+		description='Test-Publisher for ZeroMQ bus system',
+	)
+	argparser.add_argument('-H', '--host', type=str, default='localhost', help='host to connect to')
+	argparser.add_argument('-p', '--port', type=int, default='5559', help='port to connect to')
+	argparser.add_argument('-t', '--topic', type=str, default='/test', help='topic used for communication')
+
+	try:
+		args = argparser.parse_args()
+	except IOError, msg:
+		argparser.error(str(msg))
+
+	main(args.host, args.port, args.topic)
+
