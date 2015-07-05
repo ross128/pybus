393a06400bc6d9b4035d6cdb7e6fbcfdb4a9242b
ross128 <ross128@geekmail.de>
2015-07-05 10:25:57 +0200

    ported bus.py to python 3

--- bus.py
+++ bus.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import zmq
@@ -8,18 +8,20 @@ import logging.handlers
 
 class Logger(threading.Thread):
 	"""logger for all messages and events"""
-	def __init__(self, filename='bus.bz2'):
+	def __init__(self, stop_logging, filename='bus.bz2'):
 		super(Logger, self).__init__()
 		self.filename = filename
+		self.stop_logging = stop_logging
 
 		# receiving socket
 		self.context = zmq.Context.instance()
 		self.log_in = self.context.socket(zmq.PAIR)
 		self.log_in.connect("inproc://logging")
+		self.log_in.setsockopt(zmq.RCVTIMEO, 1000)
 
 		# logger parameters for stdout and compressed file
 		log_format = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
-		file_log_handler = logging.handlers.TimedRotatingFileHandler(self.filename, when='midnight', encoding='bz2', backupCount=7)
+		file_log_handler = logging.handlers.TimedRotatingFileHandler(self.filename, when='midnight', backupCount=7)
 		file_log_handler.setFormatter(log_format)
 		stream_log_handler = logging.StreamHandler()
 		stream_log_handler.setFormatter(log_format)
@@ -29,7 +31,7 @@ class Logger(threading.Thread):
 		self.logger.addHandler(stream_log_handler)
 
 	def run(self):
-		while True:
+		while not self.stop_logging.is_set():
 			try:
 				# receive message
 				message = self.log_in.recv_multipart()
@@ -42,10 +44,10 @@ class Logger(threading.Thread):
 					# subscribe/unsubscribe
 					message = message[0]
 					topic = message[1:]
-					if message[0] == '\x00':
+					if message.startswith(b'\x00'):
 						# unsubscribe
 						self.logger.info("[unsub] {%s}", topic)
-					elif message[0] == '\x01':
+					elif message.startswith(b'\x01'):
 						# subscribe
 						self.logger.info("[sub] {%s}", topic)
 					else:
@@ -55,7 +57,10 @@ class Logger(threading.Thread):
 				if e.errno == zmq.ETERM:
 					self.logger.error("socket error, stopped logging")
 					break
+				elif e.errno == zmq.EAGAIN:
+					pass
 				else:
+					print(e)
 					self.logger.error("unknown error occurred during logging")
 
 def main():
@@ -74,18 +79,19 @@ def main():
 	log_out.bind("inproc://logging")
 
 	# start logging thread
-	logger = Logger()
+	stop_logging = threading.Event()
+	logger = Logger(stop_logging)
 	logger.start()
 
 	try:
 		zmq.proxy(frontend, backend, log_out)
 	except KeyboardInterrupt:
-		print "shutting down"
+		print("shutting down")
 	finally:
 		frontend.close()
 		backend.close()
+		stop_logging.set()
 		logger.join()
-		context.term()
 
 if __name__ == "__main__":
 	main()
